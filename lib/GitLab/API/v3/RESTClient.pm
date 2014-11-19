97fd2f552be3ddc56bc813ee9f77173b5d66be28
Aran Clary Deltac <bluefeet@cpan.org>
2014-11-19 21:09:40 +0000

    Import of BLUEFEET/GitLab-API-v3-0.05.tar.gz from CPAN.
    gitpan-cpan-distribution: GitLab-API-v3
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         BLUEFEET/GitLab-API-v3-0.05.tar.gz
    gitpan-cpan-author:       BLUEFEET
    gitpan-cpan-maturity:     released

--- lib/GitLab/API/v3/RESTClient.pm
+++ lib/GitLab/API/v3/RESTClient.pm
@@ -1,5 +1,5 @@
 package GitLab::API::v3::RESTClient;
-$GitLab::API::v3::RESTClient::VERSION = '0.04';
+$GitLab::API::v3::RESTClient::VERSION = '0.05';
 =head1 NAME
 
 GitLab::API::v3::RESTClient - GitLab API v3 REST client.
